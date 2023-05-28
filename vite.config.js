c4371ddcaa96ba606fcee029cfb47fb247601192
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-05-28 08:54:12 -0700

    iltimos akalar

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/Stakepool-Frontend/",
     resolve: {alias: {util: "util/"}},
     build: {
-        chunkSizeWarningLimit: 100000000
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        },
+        chunkSizeWarningLimit: 1600,
     },
 })
