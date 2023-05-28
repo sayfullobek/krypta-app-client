fbfdba3e6e4ee17ba62a94f1f21343ac07140b13
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-05-28 08:34:01 -0700

    fgcfcxb

--- src/serverConnect/baseUrl.js
+++ src/serverConnect/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost/api/v1"
-// export const baseUrl = "https://krypta-server.herokuapp.com/api/v1"
+// export const baseUrl = "http://localhost/api/v1"
+export const baseUrl = "https://krypta-server.herokuapp.com/api/v1"
