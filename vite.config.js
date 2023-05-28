d0db86bbfc034827034f1b8eb33ddd02b22aab2d
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-05-28 08:49:12 -0700

    buildjon

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     resolve: {alias: {util: "util/"}},
+    build: {
+        chunkSizeWarningLimit: 100000000
+    },
 })
