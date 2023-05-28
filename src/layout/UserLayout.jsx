42093cee5b562005d140b528423e0ec0bb9658f8
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2023-05-28 13:43:46 -0700

    nav

--- src/layout/UserLayout.jsx
+++ src/layout/UserLayout.jsx
@@ -10,7 +10,7 @@ export const UserLayout = () => {
         <div>
             {token ? (
                 <div className={"w-100 display-flex align-items-center justify-content-center flex-column"}>
-                    <NavBar/>
+                    {/*<NavBar/>*/}
                     <main className={"w-100"}
                           style={{height: '92vh', overflow: 'auto', backgroundColor: 'rgba(226, 222, 222, 0.42)'}}>
                         <section style={{height: '92vh'}} className="section w-100
